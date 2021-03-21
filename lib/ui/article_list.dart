091ce47c5088a82f4d82fdcd4d5d042fce424823
Espen Evensen <espen@evensen.io>
2021-03-21 22:31:21 +0100

    update

---
+++ lib/ui/article_list.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/provider.dart';
+
+class ArticleList extends ConsumerWidget {
+  const ArticleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return watch(articleProvider).when(
+      loading: () => Center(child: CircularProgressIndicator()),
+      error: (e, s) {
+        print(e);
+        print(s);
+        // TODO Do you know why I get an error on Flutter web?
+        return Text('$e \n\n\n\n $s');
+      },
+      data: (articles) {
+        return SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      final listOfArticles = context.read(apiProvider);
+                      listOfArticles.sortByNid(articles);
+                    },
+                    child: Text(
+                      'Sort by node id',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final listOfArticles = context.read(apiProvider);
+                      listOfArticles.sortTitleLength(articles);
+                    },
+                    child: Text(
+                      'Sort by title.length',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: () => context.refresh(articleProvider),
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(articles[index].imageUrl!),
+                          ),
+                          title: Text('${articles[index].title!}'),
+                          trailing: Text(
+                              '(id:${articles[index].nodeId!}, length ${articles[index].title!.length})'),
+                        );
+                      },
+                      separatorBuilder: (context, index) => Divider(
+                            color: Colors.black,
+                          ),
+                      itemCount: articles.length),
+                ),
+              ),
+              Column(
+                children: [Text('Current sorting: <VALUE>')],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
